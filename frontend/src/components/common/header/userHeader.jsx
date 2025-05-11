c0e834e241b4a285cd6c730d09ef0a0beae8fc5f
belenpieroni <mbpieroni18@gmail.com>
2025-05-10 22:53:27 -0300

    link

--- frontend/src/components/common/header/userHeader.jsx
+++ frontend/src/components/common/header/userHeader.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Button, ConfigProvider, Space } from 'antd';
+import { Link } from 'react-router-dom'; 
 
 const UserHeader = () => (
     <ConfigProvider>
         <Space className='buttons-log'>
-            <Button className='header-button' href="/aboutUs" type="primary" size="large">
-                ¿Quiénes somos?
-            </Button>
-            <Button className='header-button' href="/login" type="primary" size="large">
-                Login
-            </Button>
+            <Link to="/aboutUs">  
+                <Button className='header-button' type="primary" size="large">
+                    ¿Quiénes somos?
+                </Button>
+            </Link>
+            <Link to="/login">  
+                <Button className='header-button' type="primary" size="large">
+                    Login
+                </Button>
+            </Link>
         </Space>
     </ConfigProvider>
 );
